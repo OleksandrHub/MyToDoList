9d2634c8b2eb0c8ff204c85a88fc26e6dd89f02d
OleksandrHub <sasha.klishch1@gmail.com>
2025-06-15 19:05:16 +0300

    fix addTask and editTask (date)

--- frontend/src/components/EditTask.vue
+++ frontend/src/components/EditTask.vue
@@ -37,6 +37,15 @@ export default {
             error: ''
         };
     },
+    computed: {
+        todayDate() {
+            const today = new Date();
+            const yyyy = today.getFullYear();
+            const mm = String(today.getMonth() + 1).padStart(2, '0');
+            const dd = String(today.getDate()).padStart(2, '0');
+            return `${yyyy}-${mm}-${dd}`;
+        }
+    },
     methods: {
         async editTask() {
             try {
